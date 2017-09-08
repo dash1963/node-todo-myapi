8ca35f980b07288f872711b38bd27af0f100f13b
Sergio Rueda <sergio4234@msn.com>
2017-09-08 13:47:14 -0500

    Add GET /todos/:id  route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {moment}   = require('./db/mongoose');
 var {mongoose} = require('./db/mongoose');
 
@@ -32,8 +34,25 @@ app.get('/todos', (req, res) => {
   });
 });
 
+// GET /todos/123456
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Invalid Id');
+  };
+
+  Todo.findById(id).then((todo) => {
+      if (!todo)
+      { return res.status(404).send('Id Not Found!');}
+
+      res.send({todo});
+  }).catch((error) => {
+     res.status(400).send();
+  });
+
+});
 
 
 
