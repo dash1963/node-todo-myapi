c2016a0b30f63eafafcb295e060c489ed6265550
Sergio Rueda <sergio4234@msn.com>
2017-09-10 01:03:18 -0500

    connect issue

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 // moongose.connect('mongodb://srueda:srueda@ds129394.mlab.com:29394/enigmatic-cove-43037')
 
 module.exports = {
