eae93673af34f76cbb18f0d1a2e8c73939db341c
Sergio Rueda <sergio4234@msn.com>
2017-09-10 01:10:30 -0500

    test mongoose connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,10 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-// moongose.connect('mongodb://srueda:srueda@ds129394.mlab.com:29394/enigmatic-cove-43037')
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+moongose.connect('mongodb://srueda:srueda@ds129394.mlab.com:29394/enigmatic-cove-43037' ||
+'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose: mongoose,
