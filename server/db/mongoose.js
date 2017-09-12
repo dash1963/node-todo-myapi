6ba2890c4257840bb74c87ccd22c94d0123c1c1e
Sergio Rueda <sergio4234@msn.com>
2017-09-11 21:38:04 -0500

    try another db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://srueda:srueda@ds129394.mlab.com:29394/enigmatic-cove-43037' || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://ds129394.mlab.com:29394/enigmatic-cove-43037' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://ds133104.mlab.com:33104/nodetodmyapi' || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {
